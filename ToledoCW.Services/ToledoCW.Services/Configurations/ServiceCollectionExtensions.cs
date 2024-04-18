49d6c54d0debd84e0ffa15244d0f15f2a3bbccb7
LuOliver2323 <lucaslg1607@gmail.com>
2024-04-18 20:17:06 -0300

    refact: reestruturado arquitetura

--- ToledoCW.Services/ToledoCW.Services/Configurations/ServiceCollectionExtensions.cs
+++ ToledoCW.Services/ToledoCW.Services/Configurations/ServiceCollectionExtensions.cs
@@ -1,10 +1,43 @@
+using ToledoCW.Services.Infraestructure.Repositorios;
+using ToledoCW.Services.Infraestructure;
+using ToledoCW.Services.Services;
+using Microsoft.EntityFrameworkCore;
+
 namespace ToledoCW.Services.Configurations;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
     {
-        
+        services.AddControllers();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("Padrao",
+                x => x
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+            );
+        });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddDbContext<ToledoCWContext>(options =>
+        {
+            options.UseMySQL(configuration.GetConnectionString("Mysql"));
+        });
+
+
+        services.AddScoped<INotificationService, NotificationService>();
+        services.AddScoped<IEstabelecimentoService, EstabelecimentoService>();
+
+        services.AddScoped(typeof(IRepositorioBase<>), typeof(RepositorioBase<>));
+
+        services.AddAutoMapper(typeof(Program).Assembly);
+
+
         return services;
     }
 }
--- ToledoCW.Services/ToledoCW.Services/Program.cs
+++ ToledoCW.Services/ToledoCW.Services/Program.cs
@@ -1,50 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using ToledoCW.Services.Infraestructure;
-using ToledoCW.Services.Infraestructure.Repositorios;
-using ToledoCW.Services.Services;
+using ToledoCW.Services.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddServices(builder.Configuration);
 
-builder.Services.AddControllers();
-        
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Padrao",
-        x => x
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-    );
-});
-        
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<ToledoCWContext>(options =>
-{
-    options.UseMySQL(builder.Configuration.GetConnectionString("Mysql"));
-});
-
-
-builder.Services.AddScoped<INotificationService, NotificationService>();
-builder.Services.AddScoped<IEstabelecimentoService, EstabelecimentoService>();
-
-builder.Services.AddScoped(typeof(IRepositorioBase<>), typeof(RepositorioBase<>));
-
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors("Padrao");
-
-app.MapControllers();
-        
-app.UseHttpsRedirection();
+await app.UseWebApplication(builder.Configuration);
 
 app.Run();
