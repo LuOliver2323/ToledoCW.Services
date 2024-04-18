beeaf0305da97907861d844fa0ca7d32b8498744
LuOliver2323 <lucaslg1607@gmail.com>
2024-04-18 20:31:31 -0300

    feat: add AtendenteTest

---
+++ tests/ToledoCW.Services.Unit Teste.Domain/Entidades.cs/AtendenteTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToledoCW.Services.Infraestructure.Entidades;
+
+namespace ToledoCW.Services.Unit_Teste.Domain.Entidades.cs
+{
+    public class AtendenteTest
+    {
+        [Fact]
+        public void EhValido_DeveValidarInstancia()
+        {
+            //Arrange
+            var _obj = new Atendente();
+            _obj.Nome = "Lucas";
+            _obj.Estabelecimento = 1;
+
+            //Act
+            var _result = _obj.EhValido();
+
+            //Assert
+            Assert.True(_result);
+        }
+    }
+}
---
+++ tests/ToledoCW.Services.Unit Teste.Domain/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ToledoCW.Services.UnitTeste.Domain
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
